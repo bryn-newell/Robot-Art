cbab1d1f11ae145769fb8a67b2f725ce247768a4
bryn.newell <bryn.newell@ihsmarkit.com>
2021-05-11 12:59:54 -0600

    Adding assets and basic styling set up

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div id="app">
+  <main id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </main>
 </template>
 
 <style lang="scss">
+@import './styles/main';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $gray-3;
 }
 
 #nav {
@@ -22,7 +24,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $gray-3;
 
     &.router-link-exact-active {
       color: #42b983;
